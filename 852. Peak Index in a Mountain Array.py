7522d2eac28a26458b53cfda9c9c5d84ae86277b
ngocvtd <97800384+ngocvtd@users.noreply.github.com>
2024-03-04 00:00:39 +0700

    Create 852. Peak Index in a Mountain Array.py

---
+++ 852. Peak Index in a Mountain Array.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def peakIndexInMountainArray(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(arr) - 1
+        
+        while left < right:
+            mid = left + (right - left) // 2
+            
+            if arr[mid] > arr[mid + 1]:
+                right = mid
+            else:
+                left = mid + 1
+        
+        return left
